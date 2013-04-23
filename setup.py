0dd64be7c72330dba1ca2877e7906be873ad7e10
wessie <wessie@wessie.info>
2013-04-23 22:54:25 +0200

    Initial commit with README, setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'radio_django',
+    version = '0.0alpha',
+    author = 'Wessie',
+    author_email = 'radiodjango@wessie.info',
+    description = ("The website framework used by R/a/dio"),
+    license = "BSD",
+    install_requires = [
+		"Django>=1.5",
+		"psycopg2",
+		"south",
+		"django-haystack",
+		"xapian-haystack",
+                        ],
+)
