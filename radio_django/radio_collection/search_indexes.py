def0d88ca5c8b9815636c3faa9454aa314fd5bfb
wessie <wessie@wessie.info>
2013-04-28 22:10:53 +0200

    Made relationship updates update the search engine index.

--- radio_django/radio_collection/search_indexes.py
+++ radio_django/radio_collection/search_indexes.py
@@ -1,3 +1,4 @@
+from django.db.models import signals
 from haystack import indexes
 from celery_haystack.indexes import CelerySearchIndex
 from radio_collection.models import Tags, Tracks, Albums, Artists
@@ -9,3 +10,58 @@ class TrackIndex(CelerySearchIndex, indexes.Indexable):
     def get_model(self):
         return Tracks
 
+def reindex_album(sender, instance, **kwargs):
+    index = kwargs.get('track_index', TrackIndex())
+    for track in instance.tracks_set.all():
+        index.update_object(track)
+signals.post_save.connect(reindex_album, sender=Albums)
+
+def reindex_artist(sender, instance, **kwargs):
+    index = kwargs.get('track_index', TrackIndex())
+    for track in instance.tracks_set.all():
+        index.update_object(track)
+signals.post_save.connect(reindex_artist, sender=Artists)
+
+# These are the m2m relationship signals, these are.. more work
+def m2m_artist(sender, instance, action, **kwargs):
+    if not action.startswith('post_'):
+        return
+
+    if reverse:
+        # Tags are changed. thus instance = Tags
+        index = TrackIndex()
+        for obj in instance.artists_set.all():
+            reindex_artist(sender, obj, track_index=index)
+    else:
+        # Artist side changed it. thus instance = Artists
+        reindex_artist(sender, instance)
+
+def m2m_album(sender, instance, action, reverse, **kwargs):
+    if not action.startswith('post_'):
+        return
+
+    if reverse:
+        # Tags are changed, thus instance = Tags
+        index = TrackIndex()
+        for obj in instance.albums_set.all():
+            reindex_album(sender, obj, track_index=index)
+    else:
+        # Artist side changed it, thus instance = Albums
+        reindex_album(sender, instance)
+
+def m2m_track(sender, instance, action, reverse, **kwargs):
+    if not action.startswith('post_'):
+        return
+
+    index = TrackIndex()
+    if reverse:
+        # Tags are changed, thus instance = Tags
+        for obj in instance.tracks_set.all():
+            index.update_object(obj)
+    else:
+        # Track side changed it, thus instance = Tracks
+        index.update_object(instance)
+
+signals.m2m_changed.connect(m2m_artist, sender=Artists.tags.through)
+signals.m2m_changed.connect(m2m_album, sender=Albums.tags.through)
+signals.m2m_changed.connect(m2m_track, sender=Tracks.tags.through)
