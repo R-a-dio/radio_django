61648c99aad1a81a181dc9d35ce3abeef32211e8
Wessie <github@wessie.info>
2013-05-03 19:41:55 +0200

    Enabled most of the URL confs again.

--- radio_django/radio_django/urls.py
+++ radio_django/radio_django/urls.py
@@ -8,13 +8,14 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'radio_web.views.home', name='home'),
     url(r'^irc/', 'radio_web.views.irc', name='irc'),
-    #url(r'^submit/', include('radio_collection.submit.urls')),
+    url(r'^news/', include('radio_news.urls')),
+    url(r'^submit/', include('radio_collection.submit.urls')),
     url(r'^search/', include('radio_collection.search.urls')),
     #url(r'^request/', include('radio_collection.request.urls')),
     url(r'^queue/', include('radio_stream.queue.urls')),
     url(r'^lastplayed/', include('radio_stream.lastplayed.urls')),
-    #url(r'^staff/', include('radio_users.staff.urls')),
-    #url(r'^favourites/', include('radio_users.faves.urls')),
+    url(r'^staff/', include('radio_users.staff.urls')),
+    url(r'^favourites/', include('radio_users.faves.urls')),
     #url(r'^stats/', include('radio_stream.stats.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
