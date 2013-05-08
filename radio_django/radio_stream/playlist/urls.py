4029aafc58a8c3f7dc46365af38cee8ab38fb0cc
Wessie <github@wessie.info>
2013-05-08 03:34:00 +0200

    Merged queue and lastplayed
    Their respective tastypie APIs will still be separate.

---
+++ radio_django/radio_stream/playlist/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, patterns
+
+
+urlpatterns = patterns('radio_stream.playlist.views',
+    url(r"^$", 'playlist', name="radio-playlist"),
+    url(r"^played/$", "played", name="radio-played"),
+    url(r"^played/(?P<page>\d+)/$", 'played', name="radio-played-pagination"),
+    url(r"^played/(?P<user>\w+)/$", 'played_by_name', name="radio-played-user"),
+    url(r"^played/(?P<user>\w+)/(?P<page>\d+)/$", 'played_by_name', name="radio-played-user-pagination"),
+    url(r"^queue/$", 'queue', name="radio-queue"),
+    url(r"^queue/(?P<page>\d+)/$", 'queue', name="radio-queue-pagination"),
+    url(r"^queue/(?P<user>\w+)/$", 'queue_by_name', name="radio-queue-user"),
+    url(r"^queue/(?P<user>\w+)/(?P<page>\d+)/$", "queue_by_name", name="radio-queue-user-pagination"),
+)
---
+++ radio_django/radio_stream/playlist/views.py
@@ -0,0 +1,72 @@
+from django.core.paginator import Paginator, InvalidPage
+from django.http import Http404
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
+from radio_stream.models import Played, Queue
+from radio_users.models import Djs
+from radio_users import retrieve_current_dj
+
+def playlist(request):
+    return queue(request)
+
+def queue(request, user=None, page=1):
+    if user is None:
+        user = retrieve_current_dj()
+
+    if user is None:
+        queryset = Queue.objects.none()
+    else:
+        queryset = Queue.objects.all().filter(user=user).order_by("-time").select_related()
+
+    paginator = Paginator(queryset, settings.RESULTS_PER_PAGE)
+
+    try:
+        page = paginator.page(int(page))
+    except InvalidPage:
+        raise Http404("No such page.")
+    except ValueError:
+        raise Http404("Invalid page number.")
+
+    context = RequestContext(request, {
+        "queue_page": page,
+        "queue_user": user,
+    })
+
+    return render(request, "queue/index.html",
+                  context_instance=context)
+    
+def queue_by_name(request, user, page=1):
+    user = get_object_or_404(Djs, name__iexact=user)
+
+    return queue(request, user, page)
+
+
+def played(request, user=None, page=1):
+    queryset = Played.objects.all().order_by("-time")
+
+    if not user is None:
+        queryset.filter(user=user)
+
+    paginator = Paginator(queryset, settings.RESULTS_PER_PAGE)
+
+    try:
+        page = paginator.page(int(page))
+    except InvalidPage:
+        raise Http404("Page does not exist.")
+    except ValueError:
+        raise Http404("Invalid page number.")
+
+    context = RequestContext(request, {
+        "lastplayed_page": page,
+        "lastplayed_user": user,
+    })
+
+    return render(request, "lastplayed/index.html",
+                  context_instance=context)
+
+def played_by_name(request, user, page=1):
+    user = get_object_or_404(Djs, name=user)
+
+    return played(request, user, page)
+
