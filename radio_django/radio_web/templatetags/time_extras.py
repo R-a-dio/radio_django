f70327ed554222f7399c62849b9e55b08480911d
Wessie <github@wessie.info>
2013-05-09 01:03:08 +0200

    Mockup layout + CSS for the front page.

--- radio_django/radio_web/templatetags/time_extras.py
+++ radio_django/radio_web/templatetags/time_extras.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from datetime import datetime
 
 register = Library()
 
@@ -8,3 +9,14 @@ def search_time_format(value):
         return value.strftime('%a %d %b, %H:%M')
     except AttributeError:
         return u'Never'
+
+@register.filter
+def time_format(value):
+    return value.strftime('%H:%M:%S')
+
+@register.filter
+def limit_time_format(value):
+    delta = datetime.now() - value
+    if delta >= 1:
+        return "{days:d} day{plural:s} ago".format(days=delta.days, plural='' if delta.days == 1 else 's')
+    return time_format(value)
