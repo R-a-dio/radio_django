4b295e50af619d784fe8497c6ec259da131c62c6
Wessie <github@wessie.info>
2013-05-08 03:29:35 +0200

    Changed some URLs around. Added sitetree file.
    The sitetree file is a one-off creation of our sitetree. This way
    we can generate our sitetree easily on first deployment.
    
    The URL changes accompany django-sitetree due to proper naming
    requirements. The following commits will include changes on them as
    well.

---
+++ radio_django/radio_django/radio_sitetree.py
@@ -0,0 +1,49 @@
+from sitetree.models import Tree, TreeItem
+
+def header(title, parent):
+    return dict(title=title,
+                parent=parent,
+                hint="header",
+                url="placeholder")
+
+def divider(parent):
+    return dict(title="Divider",
+                parent=parent,
+                hint="divider",
+                url="placeholder")
+
+
+tree, created = Tree.objects.get_or_create(title='radio_tree', alias='radio_tree')
+
+tree_items = [
+    dict(title="News", hint="icon-globe", url="radio-news"),
+    dict(title="Playlist", hint="icon-list", url="radio-playlist"),
+    dict(title="Submit Track", hint="icon-arrow-up", url="radio-submit"),
+    dict(title="Request", hint="icon-bullhorn", url="radio-search"),
+    dict(title="More", url="/more/", urlaspattern=False),
+    dict(title="IRC", hint="icon-comment", url="radio-irc", parent="More"),
+    dict(title="Favourites", hint="icon-heart", url="radio-faves", parent="More"),
+    divider(parent="More"),
+    header("About", parent="More"),
+    dict(title="Staff", hint="icon-user", url="radio-staff", parent="More"),
+    dict(title="Stats", hint="icon-info-sign", url="radio-stats", parent="More"),
+]
+
+if created:
+    default = dict(
+            tree=tree,
+            urlaspattern=True,
+            )
+
+    for item in tree_items:
+        # Make sure we resolve parent strings
+        if item.get('parent', None):
+            item['parent'] = TreeItem.objects.get(title=item['parent'], tree=tree)
+
+        info = dict(
+                tree=tree,
+                urlaspattern=True,
+                )
+        info.update(item)
+
+        TreeItem.objects.create(**info)
--- radio_django/radio_django/urls.py
+++ radio_django/radio_django/urls.py
@@ -8,14 +8,13 @@ from radio_django.api import container
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'radio_web.views.home', name='home'),
-    url(r'^irc/', 'radio_web.views.irc', name='irc'),
+    url(r'^$', 'radio_web.views.home', name='radio-home'),
+    url(r'^irc/', 'radio_web.views.irc', name='radio-irc'),
     url(r'^news/', include('radio_news.urls')),
     url(r'^submit/', include('radio_collection.submit.urls')),
     url(r'^search/', include('radio_collection.search.urls')),
     #url(r'^request/', include('radio_collection.request.urls')),
-    url(r'^queue/', include('radio_stream.queue.urls')),
-    url(r'^lastplayed/', include('radio_stream.lastplayed.urls')),
+    url(r'^playlist/', include('radio_stream.playlist.urls')),
     url(r'^staff/', include('radio_users.staff.urls')),
     url(r'^favourites/', include('radio_users.faves.urls')),
     #url(r'^stats/', include('radio_stream.stats.urls')),
@@ -30,7 +29,12 @@ urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')),
 )
 
-#if settings.DEBUG:
-#    from django.conf.urls.static import static
-#
-#    urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# The following code creates our sitetree if it doesn't exist yet, we assume that our
+# main site tree will be called 'radio_tree'
+
+from radio_django import radio_sitetree
