72f520a6851c4df43850c7350aa1054e5d74cf71
Wessie <github@wessie.info>
2013-05-30 21:48:33 +0200

    Made news ready for writing.

--- src/radio_news/urls.py
+++ src/radio_news/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'radio_news.views',
     url(r"^$", 'index', name="radio-news"),
-    url(r"^(?P<page>\d+)/$", 'index', name="radio-news-pagination"),
     url(r"^(?P<slug>\w+)/$", 'by_slug', name="radio-news-slug"),
     url(r"^id/(?P<news_id>\d+)/$", 'by_id', name="radio-news-id"),
 )
--- src/radio_news/views.py
+++ src/radio_news/views.py
@@ -1,54 +1,49 @@
-from django.core.paginator import Paginator, InvalidPage
-from django.conf import settings
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import Http404
+
+from endless_pagination.decorators import page_template
 
 from radio_news.models import News
 
 
-def index(request, page=1):
+@page_template("radio/news/index_page.html")
+def index(request, template="radio/news/index.html", extra_context=None):
     """
     View for the list view of news objects.
     """
     queryset = News.objects.all().order_by('-time')
 
-    paginator = Paginator(queryset, settings.RESULTS_PER_PAGE)
+    context = {
+        "news_objects": queryset,
+    }
 
-    try:
-        page = paginator.page(int(page))
-    except InvalidPage:
-        raise Http404("No such page exists.")
-    except ValueError:
-        raise Http404("Invalid page number.")
+    if extra_context is not None:
+        context.update(extra_context)
 
-    context = RequestContext(request, {
-        "news_page": page,
-    })
-
-    return render(request, "news/index.html",
-                  context_instance=context)
+    return render_to_response(template, context,
+                              context_instance=RequestContext(request))
 
 
-def detail(request, news_obj):
+def detail(request, template="radio/news/detail.html", extra_context=None):
     """
     Returns a single news post. Should also allow commenting if enabled.
     """
-    context = RequestContext(request, {
-        "news_object": news_obj,
-    })
+    context = {}
 
-    return render(request, "news/detail.html",
-                  context_instance=context)
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return render_to_response(template, context,
+                              context_instance=RequestContext(request))
 
 
 def by_slug(request, slug):
     news_obj = get_object_or_404(News, slug__iexact=slug)
 
-    return detail(request, news_obj)
+    return detail(request, extra_context={"news_object": news_obj})
 
 
 def by_id(request, news_id):
     news_obj = get_object_or_404(News, pk=news_id)
 
-    return detail(request, news_obj)
+    return detail(request, extra_context={"news_object": news_obj})
